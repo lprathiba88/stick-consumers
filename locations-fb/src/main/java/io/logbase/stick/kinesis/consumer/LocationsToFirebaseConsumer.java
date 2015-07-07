a4ddcf4b1b14b1b07ff5f1d40ab8e41583d9a158
cyberabis <babishek@gmail.com>
2015-07-07 11:14:16 +0530

    locations consumer project added

---
+++ locations-fb/src/main/java/io/logbase/stick/kinesis/consumer/LocationsToFirebaseConsumer.java
@@ -0,0 +1,67 @@
+package io.logbase.stick.kinesis.consumer;
+
+import java.net.InetAddress;
+import java.util.UUID;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessorFactory;
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionInStream;
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.KinesisClientLibConfiguration;
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.Worker;
+
+public class LocationsToFirebaseConsumer {
+
+  private static final Log LOG = LogFactory.getLog(LocationsToFirebaseConsumer.class);
+  public static final String STREAM_NAME = "stick-locations";
+  private static final String CONSUMER_NAME = "LocationsToFirebaseConsumer";
+  // Initial position in the stream when the application starts up for the first
+  // time.
+  // Position can be one of LATEST (most recent data) or TRIM_HORIZON (oldest
+  // available data)
+  private static final InitialPositionInStream SAMPLE_APPLICATION_INITIAL_POSITION_IN_STREAM = InitialPositionInStream.TRIM_HORIZON;
+  private static AWSCredentialsProvider credentialsProvider;
+
+  private static void init() {
+    // Ensure the JVM will refresh the cached IP values of AWS resources (e.g.
+    // service endpoints).
+    java.security.Security.setProperty("networkaddress.cache.ttl", "60");
+    credentialsProvider = new DefaultAWSCredentialsProviderChain();
+    try {
+      credentialsProvider.getCredentials();
+    } catch (Exception e) {
+      throw new AmazonClientException("Cannot load the credentials.", e);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    init();
+    String workerId = InetAddress.getLocalHost().getCanonicalHostName() + ":"
+        + UUID.randomUUID();
+    KinesisClientLibConfiguration kinesisClientLibConfiguration = new KinesisClientLibConfiguration(
+        CONSUMER_NAME, STREAM_NAME, credentialsProvider, workerId);
+    kinesisClientLibConfiguration
+        .withInitialPositionInStream(SAMPLE_APPLICATION_INITIAL_POSITION_IN_STREAM);
+    IRecordProcessorFactory recordProcessorFactory = new RecordProcessorFactory();
+    Worker worker = new Worker(recordProcessorFactory,
+        kinesisClientLibConfiguration);
+
+    LOG.info("Running consumer: " + CONSUMER_NAME + "|" + STREAM_NAME + "|"
+        + workerId);
+    int exitCode = 0;
+    try {
+      worker.run();
+    } catch (Throwable t) {
+      LOG.info("Caught error while running consumer: " + CONSUMER_NAME + "|"
+          + STREAM_NAME + "|" + workerId);
+      t.printStackTrace();
+      exitCode = 1;
+    }
+    System.exit(exitCode);
+  }
+
+}
---
+++ locations-fb/src/main/java/io/logbase/stick/kinesis/consumer/RecordProcessor.java
@@ -0,0 +1,142 @@
+package io.logbase.stick.kinesis.consumer;
+
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.amazonaws.services.kinesis.clientlibrary.exceptions.InvalidStateException;
+import com.amazonaws.services.kinesis.clientlibrary.exceptions.ShutdownException;
+import com.amazonaws.services.kinesis.clientlibrary.exceptions.ThrottlingException;
+import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessor;
+import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessorCheckpointer;
+import com.amazonaws.services.kinesis.clientlibrary.types.ShutdownReason;
+import com.amazonaws.services.kinesis.model.Record;
+
+/**
+ * A class used to create Record Processors for every shard.
+ *
+ * Created by kousik on 7/3/15.
+ */
+
+public class RecordProcessor implements IRecordProcessor {
+
+  private String shardId;
+  private static final Log LOG = LogFactory.getLog(RecordProcessor.class);
+
+  // Backoff and retry settings
+  private static final long BACKOFF_TIME_IN_MILLIS = 3000L;
+  private static final int NUM_RETRIES = 5;
+
+  // Checkpoint about once a minute
+  private static final long CHECKPOINT_INTERVAL_MILLIS = 60 * 000L;
+  private long nextCheckpointTimeInMillis;
+
+  private final CharsetDecoder decoder = Charset.forName("UTF-8").newDecoder();
+
+  @Override
+  public void initialize(String shardId) {
+    this.shardId = shardId;
+    LOG.info("Staring record processor for shard id: " + shardId);
+    nextCheckpointTimeInMillis = System.currentTimeMillis()
+        + CHECKPOINT_INTERVAL_MILLIS;
+  }
+
+  @Override
+  public void processRecords(List<Record> records,
+      IRecordProcessorCheckpointer checkpointer) {
+    LOG.info("Processing " + records.size() + " records from " + shardId);
+    try {
+      processRecordsWithRetries(records);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    // Checkpoint every one minute
+    if (System.currentTimeMillis() > nextCheckpointTimeInMillis) {
+      checkpoint(checkpointer);
+      nextCheckpointTimeInMillis = System.currentTimeMillis()
+          + CHECKPOINT_INTERVAL_MILLIS;
+    }
+  }
+
+  private void processRecordsWithRetries(List<Record> records) throws Exception {
+    for (Record record : records) {
+      String eventData = decoder.decode(record.getData()).toString();
+      boolean processedSuccessfully = false;
+      for (int i = 0; i < NUM_RETRIES; i++) {
+        if (businessLogic(record)) {
+          processedSuccessfully = true;
+          break;
+        }
+        // backoff if we encounter an exception and try again.
+        try {
+          Thread.sleep(BACKOFF_TIME_IN_MILLIS);
+        } catch (InterruptedException e) {
+          LOG.debug("Interrupted sleep", e);
+        }
+      }
+      if (!processedSuccessfully) {
+        LOG.error("Couldn't process record " + record
+            + ". Skipping the record.");
+      }
+    }
+  }
+
+  private boolean businessLogic(Record record) {
+    // TODO
+    return false;
+  }
+
+  @Override
+  public void shutdown(IRecordProcessorCheckpointer checkpointer,
+      ShutdownReason reason) {
+    if (reason == ShutdownReason.TERMINATE) {
+      checkpoint(checkpointer);
+    }
+    LOG.info("Shutting down record processor for shard id: " + shardId);
+  }
+
+  /**
+   * Checkpoint with retries. This will update the checkpoint in DynamoDB for
+   * every record processed. This will keep the lease alive for the shard.
+   *
+   * @param checkpointer
+   */
+  private void checkpoint(IRecordProcessorCheckpointer checkpointer) {
+    LOG.info("Checkpointing shard " + shardId);
+    for (int i = 0; i < NUM_RETRIES; i++) {
+      try {
+        checkpointer.checkpoint();
+        break;
+      } catch (ShutdownException se) {
+        // Ignore checkpoint if the processor instance has been shutdown (fail
+        // over).
+        LOG.info("Caught shutdown exception, skipping checkpoint.", se);
+        break;
+      } catch (ThrottlingException e) {
+        // Backoff and re-attempt checkpoint upon transient failures
+        if (i >= (NUM_RETRIES - 1)) {
+          LOG.error("Checkpoint failed after " + (i + 1) + "attempts.", e);
+          break;
+        } else {
+          LOG.info("Transient issue when checkpointing - attempt " + (i + 1)
+              + " of " + NUM_RETRIES, e);
+        }
+      } catch (InvalidStateException e) {
+        LOG.error(
+            "Cannot save checkpoint to the DynamoDB table used by the Amazon Kinesis Client Library.",
+            e);
+        break;
+      }
+      try {
+        Thread.sleep(BACKOFF_TIME_IN_MILLIS);
+      } catch (InterruptedException e) {
+        LOG.debug("Interrupted sleep", e);
+      }
+    }
+  }
+
+  
+}
